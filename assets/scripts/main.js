fd988aa1aa14328688eac65f21e50bfb83353fa8
Mahmoud Abuyoussef <mahmoudabuyoussef5@gmail.com>
2025-01-16 11:35:50 +0200

    Add: Header Section & Add: Dark Mode & Add: Light Mode

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+document.getElementById("toggle-nav-menu").onclick = () => {
+  document.getElementById("nav-menu").classList.toggle("open");
+};
+
+document.getElementById("toggle-theme").onclick = () => {
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.remove("dark-theme");
+    document.getElementById("toggle-theme").src = "assets/icons/dark.webp";
+  } else {
+    document.body.classList.add("dark-theme");
+    document.getElementById("toggle-theme").src = "assets/icons/light.webp";
+  }
+};
